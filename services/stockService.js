7fe31fae043cd6b99afda8f07413252f62c2159a
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2021-09-30 09:05:09 +0530

    Add new stock api features

--- services/stockService.js
+++ services/stockService.js
@@ -1,17 +1,31 @@
 import axios from 'axios';
 import { IEX_PUBLISHABLE } from '../constants/keys.js';
 
-const getStockPrice = async symbol => {
+const root = async url => {
   try {
-    const response = await axios.get(
-      `https://sandbox.iexapis.com/stable/stock/${symbol}/price?token=${IEX_PUBLISHABLE}`
-    );
-    console.log('$' + response.data);
-    return '$' + response.data;
+    const response = await axios.get(url);
+    console.log(response.data);
+    return { success: true, ...response.data };
   } catch (e) {
     console.log(e.response.status, e.response.data);
     console.log(e.message);
+    return { success: false };
   }
 };
 
-export { getStockPrice };
+const getStockPrice = symbol =>
+  root(
+    `https://sandbox.iexapis.com/stable/stock/${symbol}/price?token=${IEX_PUBLISHABLE}`
+  );
+
+const getCompanyDetails = symbol =>
+  root(
+    `https://sandbox.iexapis.com/stable/stock/${symbol}/company?token=${IEX_PUBLISHABLE}`
+  );
+
+const getQuote = symbol =>
+  root(
+    `https://sandbox.iexapis.com/stable/stock/${symbol}/quote?token=${IEX_PUBLISHABLE}`
+  );
+
+export { getStockPrice, getCompanyDetails, getQuote };

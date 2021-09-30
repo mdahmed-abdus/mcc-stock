ef1fef13a6b336e3714645eb3d9f8b9328115c1d
Mohd Ahmed Abdus <mdahmed.abdus@gmail.com>
2021-09-30 12:29:31 +0530

    Add stock to favourites

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -10,20 +10,42 @@ import {
 } from 'react-native';
 import SearchBox from '../components/SearchBox';
 import { scaleSize } from '../constants/layout';
+import { QuoteModel } from '../models/QuoteModel';
+import { getQuote } from '../services/stockService';
 
 function FavouritesScreen(props) {
+  const [quotes, setQuotes] = useState([]);
   const [symbol, setSymbol] = useState('');
 
-  const handleChangeSearchText = async text => setSymbol(text);
-
-  const handleOnPress = () => {
+  const handleOnPress = async () => {
     Keyboard.dismiss();
     if (symbol.length === 0) {
       Alert.alert('Invalid symbol', 'Please enter stock symbol');
       return;
     }
 
+    let alreadyAdded = false;
+    quotes.forEach(q => {
+      alreadyAdded = q.symbol.value === symbol.toUpperCase();
+    });
+    if (alreadyAdded) {
+      Alert.alert('Already a favourite', `${symbol} is a favourite.`);
+      return;
+    }
+
     console.log('handleOnPress -', symbol);
+    const quote = await getQuote(symbol);
+
+    if (!quote.success) {
+      Alert.alert('Invalid symbol', 'The stock symbol entered is invalid');
+      return;
+    }
+
+    Object.keys(QuoteModel).forEach(k => {
+      QuoteModel[k] = { ...QuoteModel[k], value: quote[k] };
+    });
+
+    setQuotes(quotes.concat([{ ...QuoteModel }]));
   };
 
   const loadPressableTextStyle = pressed =>
@@ -34,6 +56,11 @@ function FavouritesScreen(props) {
   return (
     <TouchableWithoutFeedback style={{ flex: 1 }} onPress={Keyboard.dismiss}>
       <View style={{ padding: 10 }}>
+        {quotes.map((q, i) => (
+          <Text key={i} style={{ color: DefaultTheme.colors.card, padding: 5 }}>
+            {q.companyName.value} ({q.symbol.value})
+          </Text>
+        ))}
         <SearchBox
           placeholder="Search stock by symbol (ex: tsla)"
           handleChangeSearchText={text => setSymbol(text)}
